47a0b52a4e5f99c7c36abcabcfcbccef4b565fea
Carlos <151273940+realcarloss@users.noreply.github.com>
2024-04-08 02:01:21 +0300

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,14 +1,14 @@
 module.exports = {
-  token: "MTIyNjU4MTYxNTI5MjA1NTU2Mw.GmEtrO.31o9t3J1JrewnzXg3ZdW50goIxMXB84RFXApA0", //Token do bot
+  token: "", //Bot Token
   port: "80",
-  prefix: "*", //Prefixo do bot
-  client_id: "1226581615292055563", // ID do bot
-  client_secret: "oks7rNHyhIj2ing6vwSMjUEEfJrjGpAk", //Client secret do bot
-  redirect_uri: "http://46.31.77.253:80", //URL de redirecionamento
-  idserver: "1200489428053925888", //ID do Servidor
-  idrole: "", ////ID do Cargo para ser adicionado ao se verificar
-  webhook: "https://discord.com/api/webhooks/1226060250151522305/2MrDIuc8rDkLcp4rAkbz6ZxrvuIbINEhOu9n_cuxls4uNbfEi9ukm3Q_lZKHsZmgLePe", //wehbook de logs
-  webhookBackup: "https://discord.com/api/webhooks/1226060250151522305/2MrDIuc8rDkLcp4rAkbz6ZxrvuIbINEhOu9n_cuxls4uNbfEi9ukm3Q_lZKHsZmgLePe", //wehbook de backup do object.json
-  authLink: `https://discord.com/oauth2/authorize?client_id=1226581615292055563&response_type=code&redirect_uri=http%3A%2F%2F46.31.77.253%3A80&scope=identify+guilds+guilds.join`, //Link Auth do bot
-  owners: ["1049023653536337920", "", ""], //ID dos donos do bot
+  prefix: "", //Bot Prefix
+  client_id: "", //Client ID
+  client_secret: "", //Client Secret
+  redirect_uri: "https://123456789.replit.dev:80", //Changing the Port Part!
+  idserver: "", //Server ID Main
+  idrole: "", //Role ID Main
+  webhook: "", //Auth Log
+  webhookBackup: "", //wehbook de backup do object.json
+  authLink: ``, //Auth Link
+  owners: ["1049023653536337920", "", ""], //Owner ID
 };
